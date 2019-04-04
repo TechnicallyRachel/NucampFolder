6cd05d6d023f36c57ecc00a29d0127c263a272b8
Rachel Giles <paintmetothemoon@gmail.com>
2019-04-03 22:37:37 -0700

    Update repeatIfElse.js
    changing things around

--- repeatIfElse.js
+++ repeatIfElse.js
@@ -1,11 +1,11 @@
 //repeat function using if else
 
-function repeat(string, times) {
-  if(times < 0) 
+function repeat(string, num) {
+  if(num < 0) 
     return "";
-  if(times === 1) 
+  if(num == 1) // would it matter if this were (times === 1)?
     return string;
   else 
-    return string + repeat(string, times - 1);
+    return string + repeat(string, num - 1);
 }
 repeat("x", 5);

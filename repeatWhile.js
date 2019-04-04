7c09c268238f48bdaf6a7d8ad52ee4a96a85533e
Rachel Giles <paintmetothemoon@gmail.com>
2019-04-03 22:46:30 -0700

    Update repeatWhile.js
    Experimenting

--- repeatWhile.js
+++ repeatWhile.js
@@ -9,3 +9,14 @@ function repeat(string, num) {
   return i;
 }
 repeat("x", 3);
+
+
+// ---Experimental--
+// function repeat(string, num) {
+//   let i = "";
+//   for (num > 0, i += string, num--) {
+
+//   }
+//   return i;
+// }
+// repeat("x", 3);
